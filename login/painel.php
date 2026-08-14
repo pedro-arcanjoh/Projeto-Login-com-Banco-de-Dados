3f731e79e0464bc32f8ce356bd5911258a7c07a6
pedro-arcanjoh <pedroh190508@gmail.com>
2026-08-14 10:42:35 -0300

    Add files via upload

---
+++ login/painel.php
@@ -0,0 +1,22 @@
+<?php
+
+include('protect.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    Bem vindo ao painel, <?php echo $_SESSION['nome']?>
+
+    <div>
+        <a href="logout.php">Sair</a>
+    </div>
+</body>
+</html>
